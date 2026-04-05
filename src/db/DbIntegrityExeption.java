19d8b3afa73e2e811e42056acdd82fc8fdb32f05
J. Victor Pereira <victorifpb78@gmail.com>
2026-04-05 19:14:54 -0300

    Start project

---
+++ src/db/DbIntegrityExeption.java
@@ -0,0 +1,13 @@
+package db;
+
+public class DbIntegrityExeption extends RuntimeException {
+ 
+	private static final long serialVersionUID = 1L;
+	
+	
+	public DbIntegrityExeption(String msg) {
+		super(msg); 
+	}
+	
+
+}
